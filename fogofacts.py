e3f0127db9e2474462139385e3242fddd19af090
Travis Peterson <travis.peterson@ave81.com>
2017-05-19 15:41:07 -0500

    moar cleanup

--- fogofacts.py
+++ fogofacts.py
@@ -1,71 +1,71 @@
 from random import choice
 
 
-class CatFacts(object):
+class FogoFacts(object):
 
-  facts = []
-  subscribers = {} # Format: {user_id: {fact_key: fact_count}}
-  fact_count = 0
+    facts = []
+    subscribers = {}  # Format: {user_id: {fact_key: fact_count}}
+    fact_count = 0
 
-  def __init__(self):
-    self.facts = open('facts.txt').readlines()
+    def __init__(self):
+        self.facts = open('facts.txt').readlines()
 
-    self.fact_count = len(self.facts)
+        self.fact_count = len(self.facts)
 
-    subs = open('subscribers.txt')
-    for line in subs:
-      chunks = line.split(',')
+        subs = open('subscribers.txt')
+        for line in subs:
+            chunks = line.split(',')
 
-      received_facts = {}
-      for fact in chunks[1:]:
-        fact = fact.split(':')
-        received_facts[fact[0]] = int(fact[1])
+            received_facts = {}
+            for fact in chunks[1:]:
+                fact = fact.split(':')
+                received_facts[fact[0]] = int(fact[1])
 
-      self.subscribers[chunks[0]] = received_facts
+            self.subscribers[chunks[0]] = received_facts
 
-  def add_subscriber(self, user_id):
-    if user_id in self.subscribers: return
+    def add_subscriber(self, user_id):
+        if user_id in self.subscribers: return
 
-    self.subscribers[user_id] = {}
+        self.subscribers[user_id] = {}
 
-  def remove_subscriber(self, user_id):
-    if user_id not in self.subscribers: return
-    self.subscribers.pop(user_id)
+    def remove_subscriber(self, user_id):
+        if user_id not in self.subscribers: return
+        self.subscribers.pop(user_id)
 
-  def get_fact(self, subscriber = None):
-    if subscriber != None and subscriber not in self.subscribers:
-      subscriber = None
+    def get_fact(self, subscriber = None):
+        if subscriber is None and subscriber not in self.subscribers:
+            subscriber = None
 
-    fact = choice(range(self.fact_count))
+        fact = choice(range(self.fact_count))
 
-    i = 0
-    while subscriber != None and \
-          fact in self.subscribers[subscriber] and \
-          self.subscribers[subscriber][fact] == max(self.subscribers[subscriber].values()) and \
-          i < 10:
-      i += 1
-      fact = choice(range(self.fact_count))
+        i = 0
+        while subscriber is None and \
+                  fact in self.subscribers[subscriber] and \
+                  self.subscribers[subscriber][fact] == max(self.subscribers[subscriber].values()) and \
+                  i < 10:
+            i += 1
+            fact = choice(range(self.fact_count))
 
-    if subscriber != None:
-      if fact not in self.subscribers[subscriber]:
-        self.subscribers[subscriber][fact] = 1
-      else:
-        self.subscribers[subscriber][fact] += 1
+        if subscriber is None:
+            if fact not in self.subscribers[subscriber]:
+                self.subscribers[subscriber][fact] = 1
+            else:
+                self.subscribers[subscriber][fact] += 1
 
-    return self.facts[fact]
+        return self.facts[fact]
 
-  def write_subscribers(self):
-    subs = open('subscribers.txt', 'w')
-    for user, facts in self.subscribers.items():
-      print('  Writing {}.'.format(user))
-      subs.write(user)
-      for fact, count in facts.items():
-        subs.write(',' + str(fact) + ':' + str(count))
-      subs.write('\n')
-    subs.close
+    def write_subscribers(self):
+        subs = open('subscribers.txt', 'w')
+        for user, facts in self.subscribers.items():
+            print('  Writing {}.'.format(user))
+            subs.write(user)
+            for fact, count in facts.items():
+                subs.write(',' + str(fact) + ':' + str(count))
+            subs.write('\n')
+        subs.close
 
-  def get_subscribers(self):
-    return self.subscribers.keys()
+    def get_subscribers(self):
+        return self.subscribers.keys()
 
-  def list_subscribers(self):
-    return '```' + '\n'.join(self.subscribers.keys()) + '```'
+    def list_subscribers(self):
+        return '```' + '\n'.join(self.subscribers.keys()) + '```'
--- fogofactsbot.py
+++ fogofactsbot.py
@@ -50,7 +50,7 @@ def save_subs():
 #
 # signal.signal(signal.SIGTERM, sigterm_handler)
 
-TOKEN = os.environ.get('TOKEN', None)  # found at https://api.slack.com/web#authentifogoion
+TOKEN = os.environ.get('FOGOFACTSECRET', None)  # found at https://api.slack.com/web#authentifogoion
 S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY', None)
 S3_SECRET_KEY = os.environ.get('S3_SECRET_KEY', None)
 
@@ -66,45 +66,45 @@ cf = CatFacts()
 
 sc = SlackClient(TOKEN)
 if sc.rtm_connect() is True:
-  print('Connected.')
+    print('Connected.')
 
-  sc.api_call("im.list")
+    sc.api_call("im.list")
 
-  while True:
-    response = sc.rtm_read()
-    for part in response:
-      if 'ims' in part:
-        channels = part['ims']
-        if part['type'] == 'message':
-            if '<@U1MKHKV8U>' in part['text']:
-              if 'unsubscribe' in part['text'].lower():
-                cf.remove_subscriber(part['channel'].strip())
-                save_subs()
-                sc.api_call("chat.postMessage", channel=part['channel'], text="We're sorry to see you go.", username=NAME, icon_emoji=':crying_fogo_face:')
-            elif 'subscribe' in part['text'].lower():
-                cf.add_subscriber(part['channel'].strip())
-                save_subs()
-                sc.api_call("chat.postMessage", channel=part['channel'], text="Thanks for subscribing to fogo facts! Here's your complimentary first fogo fact!", username=NAME, icon_emoji=':iseewhatyoudidthere:')
-                sc.api_call("chat.postMessage", channel=part['channel'], text=cf.get_fact(part['channel'].strip()), username=NAME, icon_emoji=':iseewhatyoudidthere:')
-            elif 'fact' in part['text'].lower():
-                sc.api_call("chat.postMessage", channel=part['channel'], text=cf.get_fact(part['channel'].strip()), username=NAME, icon_emoji=get_icon_emoji())
-                save_subs()
-            elif 'list' in part['text'].lower() and part['user'] == 'U0PDQ1P2R':
-                sc.api_call("chat.postMessage", channel=part['channel'], text=cf.list_subscribers(), username=NAME, icon_emoji=get_icon_emoji())
-            else:
-                sc.api_call("chat.postMessage", channel=part['channel'], text=usage, username=NAME, icon_emoji=get_icon_emoji())
+    while True:
+        response = sc.rtm_read()
+        for part in response:
+            if 'ims' in part:
+                channels = part['ims']
+            if part['type'] == 'message':
+                if '<@U1MKHKV8U>' in part['text']:
+                    if 'unsubscribe' in part['text'].lower():
+                        cf.remove_subscriber(part['channel'].strip())
+                        save_subs()
+                        sc.api_call("chat.postMessage", channel=part['channel'], text="We're sorry to see you go.", username=NAME, icon_emoji=':crying_fogo_face:')
+                    elif 'subscribe' in part['text'].lower():
+                        cf.add_subscriber(part['channel'].strip())
+                        save_subs()
+                        sc.api_call("chat.postMessage", channel=part['channel'], text="Thanks for subscribing to fogo facts! Here's your complimentary first fogo fact!", username=NAME, icon_emoji=':smile_fogo:')
+                        sc.api_call("chat.postMessage", channel=part['channel'], text=cf.get_fact(part['channel'].strip()), username=NAME, icon_emoji=':smile_fogo:')
+                    elif 'fact' in part['text'].lower():
+                        sc.api_call("chat.postMessage", channel=part['channel'], text=cf.get_fact(part['channel'].strip()), username=NAME, icon_emoji=get_icon_emoji())
+                        save_subs()
+                    elif 'list' in part['text'].lower() and part['user'] == 'U0PDQ1P2R':
+                        sc.api_call("chat.postMessage", channel=part['channel'], text=cf.list_subscribers(), username=NAME, icon_emoji=get_icon_emoji())
+                    else:
+                        sc.api_call("chat.postMessage", channel=part['channel'], text=usage, username=NAME, icon_emoji=get_icon_emoji())
 
-    if 0 <= datetime.now(timezone('Australia/Sydney')).time().hour < 1 and posted is True: #midnight to 1am
-        print('It\'s a new day.')
-        posted = False
-    if 15 <= datetime.now(timezone('Australia/Sydney')).time().hour < 17 and posted is False: #3pm to 5pm
-        print('It\'s fogo fact time!')
-        posted = True
-    for channel in cf.get_subscribers():
-        print('Sending a fact to {}.'.format(channel))
-        sc.api_call("chat.postMessage", channel=channel, text=cf.get_fact(channel), username=NAME, icon_emoji=get_icon_emoji())
-        save_subs()
+        if 0 <= datetime.now(timezone('Australia/Sydney')).time().hour < 1 and posted == True: #midnight to 1am
+            print('It\'s a new day.')
+            posted = False
+        if 15 <= datetime.now(timezone('Australia/Sydney')).time().hour < 17 and posted == False: #3pm to 5pm
+            print('It\'s fogo fact time!')
+            posted = True
+            for channel in cf.get_subscribers():
+                print('Sending a fact to {}.'.format(channel))
+                sc.api_call("chat.postMessage", channel=channel, text=cf.get_fact(channel), username=NAME, icon_emoji=get_icon_emoji())
+            save_subs()
 
         sleep(1)
-    else:
-      print('Connection Failed, invalid token?')
+  else:
+    print('Connection Failed, invalid token?')
